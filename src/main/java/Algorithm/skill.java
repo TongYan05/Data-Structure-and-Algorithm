47f774933e0e392339982c9f9441d9def0963b54
Yan Tong <19892432281@163.com>
2026-09-24 23:22:55 +1000

    skill

--- src/main/java/Algorithm/skill.java
+++ src/main/java/Algorithm/skill.java
@@ -15,6 +15,23 @@ public class skill {
     }
 
 
+    //287. 寻找重复数
+    class Solution2 {
+        public int findDuplicate(int[] nums) {
+            int slow=0;
+            int fast=0;
+            do{
+                slow=nums[slow];
+                fast=nums[nums[fast]];
+            }while(slow!=fast);//slow == fast
+            slow=0;
+            while(slow!=fast){
+                slow=nums[slow];
+                fast=nums[fast];
+            }
+            return slow;
+        }
+    }
 
 
 
