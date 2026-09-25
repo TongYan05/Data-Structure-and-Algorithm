816eb63a06e0bc76a590c66cdb9c8db314017375
Yan Tong <19892432281@163.com>
2026-09-26 09:58:14 +1000

    leetcode

--- src/main/java/Algorithm/Stack.java
+++ src/main/java/Algorithm/Stack.java
@@ -1,7 +1,6 @@
 package Algorithm;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 public class Stack {
     static void main(String[] args) {
@@ -17,5 +16,51 @@ public class Stack {
         System.out.println(deque1.isEmpty());
         System.out.println(deque1.poll());
         System.out.println(deque1.pop());// NullPointException!!!!! pop pop pop pop pop pop!!!!
+
+
+
+
+        //347. 前 K 个高频元素
+        class Solution1 {
+            public int[] topKFrequent(int[] nums, int k) {
+                Map<Integer,Integer> map=new HashMap<>();
+                for(int i : nums){
+                    if(map.containsKey(i)){
+                        map.put(i,map.get(i)+1);
+                    }else{
+                        map.put(i,1);
+                    }
+                }
+                PriorityQueue<Integer> p=new PriorityQueue<>((a, b)->map.get(a)-map.get(b));
+                for(int x : map.keySet()){
+                    if(p.size()<k){// the = will make the size is p.size()+1
+                        p.offer(x);
+                        continue;
+                    }
+
+                    if(map.get(x)>map.get(p.peek())){
+                        p.poll();
+                        p.offer(x);
+                    }
+
+                }
+
+                int[] result=new int[p.size()];
+                for(int i=0;i<result.length;i++){
+                    result[i]=p.poll();
+                }
+
+                return result;
+
+            }
+        }
+
+
+
+
+
+
+
+
     }
 }
